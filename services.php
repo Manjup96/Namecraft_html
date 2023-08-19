433bf2f02f7db3d444317bc1c9630c92fc72fba2
Uppala Hemanth <uppalahemanth4@gmail.com>
2023-08-19 17:58:21 +0530

    hemanth

--- services.php
+++ services.php
@@ -15,16 +15,25 @@ html {
        
     }
 }
-.overlay{
-    position: absolute;
-    top: 0;
-    left: 4%;
-    right: 4%;
-    bottom: 17%;
-    opacity: 0;
-    transition: opacity 0.4s ease-in-out;
-    background:white;
-    cursor: pointer;
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  opacity: 0;
+  transition: opacity 0.4s ease-in-out;
+  background: white;
+  cursor: pointer;
+}
+
+.image-container {
+  position: relative;
+  overflow: hidden;
+}
+
+.image-container:hover .overlay {
+  opacity: 0.8;
 }
 .content {
 position: absolute;
@@ -38,9 +47,7 @@ border: 1px solid;
 font-family: verdana;
 text-align: center;
 }
-.container:hover .overlay{
-    opacity:0.8;
-}
+
 
 </style>
 
@@ -60,68 +67,73 @@ text-align: center;
             </div>
 </div>
 <div class="container">
-  
-<div class="row">
-
-    <div class="col-lg-4 col-md-6 col-sm-6 text-center" style="margin-top:50px">
-    
-        <a href="babyname.php"><img src="images/babyname numerology.jpg" style="width:350px;height:350px; border: 2px solid black;"></a>
-        <div class="overlay">
+  <div class="row">
+    <div class="col-lg-4 col-md-6 col-sm-6 text-center" style="margin-top: 50px">
+      <a href="babyname.php">
+        <div class="image-container">
+          <img src="images/babyname numerology.jpg" style="width: 350px; height: 350px; border: 2px solid black;">
+          <div class="overlay">
             <div class="content">
-                <p style="margin-top:10px;">Select option</p>
+              <p style="margin-top: 10px;">Select option</p>
+            </div>
+          </div>
+        </div>
+      </a>
+      <h2>
+        <b class="text-center pt-3" style="font-size: 16px; font-family: montserrat;">Baby Name Correction</b>
+      </h2>
+      <b class="text-center" style="font-size: 16px;">Rs: 35.00</b>
+    </div>
+    
+    <div class="col-lg-4 col-md-6 col-sm-6 text-center" style="margin-top: 50px">
+    <a href="luckydate.php">
+        <div class="image-container">
+            <img src="images/C section delivery date.jpg" style="width: 350px; height: 350px; border: 2px solid black;">
+            <div class="overlay">
+                <div class="content">
+                    <p style="margin-top: 10px;">Select option</p>
+                </div>
             </div>
         </div>
-   
-        <h2>
-            <b class="text-center pt-3" style="font-size: 16px; font-family:montserrat;">Baby Name Correction</b>
-        </h2>
-        <b class="text-center" style="font-size: 16px;">Rs:35.00</b>
-    </div>
-    
-    <div class="col-lg-4 col-md-6 col-sm-6 text-center" style="margin-top:50px">
-   
-    <a href="luckydate.php"> <img src="images/C section delivery date.jpg" style="width:350px;height:350px; border: 2px solid black;"></a>
-        <div class="overlay">
-        <div class="content">
-            <p style="margin-top:10px;">Select option</p>
-        </div>
-    </div>
-  
-        <h2>
-            <b class="text-center pt-3" style="font-size: 16px; font-family:montserrat;">Lucky Date Delivery</b>
-        </h2>
-        <b class="text-center" style="font-size: 16px;">Rs:45.00</b>
-    </div>
+    </a>
+    <h2>
+        <b class="text-center pt-3" style="font-size: 16px; font-family: montserrat;">Lucky Date Delivery</b>
+    </h2>
+    <b class="text-center" style="font-size: 16px;">Rs: 45.00</b>
+</div>
 
-    <div class="col-lg-4 col-md-6 col-sm-6 text-center" style="margin-top:50px">
-    
-        <a href="luckyname.php"><img src="images/LUCKY ALPHABETS.jpg" style="width:350px;height:350px; border: 2px solid black;"></a>
-        <div class="overlay">
-        <div class="content">
-            <p style="margin-top:10px;">Select option</p>
+<div class="col-lg-4 col-md-6 col-sm-6 text-center" style="margin-top: 50px">
+    <a href="luckyname.php">
+        <div class="image-container">
+            <img src="images/LUCKY ALPHABETS.jpg" style="width: 350px; height: 350px; border: 2px solid black;">
+            <div class="overlay">
+                <div class="content">
+                    <p style="margin-top: 10px;">Select option</p>
+                </div>
+            </div>
         </div>
-    </div>
-   
-        <h2>
-            <b class="text-center pt-3" style="font-size: 16px; font-family:montserrat;">Lucky Name Correction</b>
-        </h2>
-        <b class="text-center" style="font-size: 16px;">Rs:45.00</b>
-    </div>
-
-    <div class="col-lg-4 col-md-6 col-sm-6 text-center" id="a" style="margin-top:50px">
-       
-    <a href="nameanalysis.php"> <img src="images/Name analysis.jpg" style="width:350px;height:350px; border: 2px solid black;"></a>
-    <div class="overlay">
-        <div class="content">
-            <p style="margin-top:10px;">Select option</p>
+    </a>
+    <h2>
+        <b class="text-center pt-3" style="font-size: 16px; font-family: montserrat;">Lucky Name Correction</b>
+    </h2>
+    <b class="text-center" style="font-size: 16px;">Rs: 45.00</b>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-6 text-center" style="margin-top: 50px">
+    <a href="nameanalysis.php">
+        <div class="image-container">
+            <img src="images/Name analysis.jpg" style="width: 350px; height: 350px; border: 2px solid black;">
+            <div class="overlay">
+                <div class="content">
+                    <p style="margin-top: 10px;">Select option</p>
+                </div>
+            </div>
         </div>
-    </div>
-  
-        <h2>
-            <b class="text-center pt-3" style="font-size: 16px; font-family:montserrat;">Name Analysis</b>
-        </h2>
-        <b class="text-center" style="font-size: 16px;">Rs:100.00</b>
-    </div>
+    </a>
+    <h2>
+        <b class="text-center pt-3" style="font-size: 16px; font-family: montserrat;">Name Analysis</b>
+    </h2>
+    <b class="text-center" style="font-size: 16px;">Rs: 100.00</b>
+</div>
     </div>
     </div>
     <?php include("footer.php"); ?>
