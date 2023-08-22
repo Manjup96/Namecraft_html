24da8035cfed4243b91b57a29a86b06a84c221d0
Uppala Hemanth <uppalahemanth4@gmail.com>
2023-08-22 16:48:26 +0530

    hemanth

--- footer.php
+++ footer.php
@@ -3,6 +3,33 @@
   background-color:#eee5ef;
 }
 </style>
+<style>
+    .footer-links a {
+        text-decoration: none; /* Remove underline */
+        color: #000000;
+        font-size: 14px;
+        margin-bottom: 5px;
+        display: inline-block;
+    }
+
+    .footer-links a:hover {
+        color: #61156B; /* Change color on hover */
+    }
+
+    /* Center-align text in #footer only in mobile view (up to 767px) */
+@media (max-width: 767px) {
+    #footer {
+        text-align: center;
+    }
+    li {
+        text-align: center;
+    }
+}
+p{font-size:10px;
+margin-top:10px;
+color:lightgray;}
+
+</style>
 
 
 
@@ -14,30 +41,32 @@
         <div class="row">
           
 
-        <div class="col-md-4 " >
-          <div style="margin-left:20px">
-            <h4 style="font-size:12px;color:#61156B ;" class="pb-2">| SERVICES BY NAMECRAFT</h4><br>
-            <div class="text-center" style="font-family:Open Sans, sans-serif;">
-              <ul class="footer-links list-unstyled text-start">
-                <li><a href="nameanalysis.php" style="margin-bottom:5px;font-size:14px;color:#000000">Name Analysis</a></li><br>
-                <li><a href="luckydate.php" style="margin-bottom:5px;font-size:14px;color:#000000">Lucky date delivery</a></li><br>
-                <li><a href="babyname.php" style="margin-bottom:5px;font-size:14px;color:#000000">Baby name correction</a></li><br>
-                <li><a href="luckyname.php" style="margin-bottom:5px;font-size:14px;color:#000000">Lucky name correction</a></li><br>
-              </ul>
-            </div>
-            </div>
-          </div>
+        
+<div class="col-md-4 mb-3" >
+    <div >
+        <h4 style="font-size: 12px; color: #61156B;" class="pb-2">| SERVICES BY NAMECRAFT</h4><br>
+        <div class="text-center" style="font-family: Open Sans, sans-serif;">
+            <ul class="footer-links list-unstyled text-start"  >
+                <li><a href="nameanalysis.php">Name Analysis</a></li><br>
+                <li><a href="luckydate.php">Lucky date delivery</a></li><br>
+                <li><a href="babyname.php">Baby name correction</a></li><br>
+                <li><a href="luckyname.php">Lucky name correction</a></li><br>
+            </ul>
+        </div>
+    </div>
+</div>
+
 
           <!-- <div class="col-md-1"></div> -->
-          <div class="col-md-4 ">
-            <div style="margin-left:20px">
+          <div class="col-md-4 mb-3">
+            <div >
             <h4 style="font-size:12px;color:#61156B ;" class="pb-1">| MORE INFO</h4><br>
             <div class="text-center" style="font-family:Open Sans, sans-serif;">
-              <ul class="footer-links list-unstyled text-start">
-                <li><a href="index.php" style="margin-bottom:5px;font-size:14px;color:#000000">Terms of service</a></li><br>
-                <li><a href="refund_policy.php" style="margin-bottom:5px;font-size:14px;color:#000000"> Refund policy</a></li><br>
-                <li><a href="privacy_policy.php" style="margin-bottom:5px;font-size:14px;color:#000000">Privacy policy</a></li><br>
-                <li><a href="contact.php" style="margin-bottom:5px;font-size:14px;color:#000000">Contact us</a></li><br>
+              <ul class="footer-links list-unstyled text-start" >
+                <li><a href="index.php" >Terms of service</a></li><br>
+                <li><a href="refund_policy.php" > Refund policy</a></li><br>
+                <li><a href="privacy_policy.php" >Privacy policy</a></li><br>
+                <li><a href="contact.php" >Contact us</a></li><br>
               </ul>
             </div>
             </div>
@@ -49,21 +78,36 @@
             <p style="font-size:18px;color:white">Number 664, 5 ft Cross, 4th block, Koramangala, Near maharaja signal, Bengaluru – 560034</p>
           </div> -->
          
-          <div class="col-md-4  col-12 ">
-            <img src="images/final-logo.png" class="img-fluid" alt="Logo" style="height:80px;width:300px;">
-          </div>
+         <div class="col-md-4 col-12 text-center mb-3" style="max-width: 300px; margin: 0 auto;">
+    <img src="images/final-logo.png" class="img-fluid logo" alt="Logo" style="height: 80px; width: 100%;">
+</div>
+
         </div>
 
           <div class="row">
             <div class="col-md-3"></div>
-            <div class="col-md-6">
+            <style>
+    .social-icon {
+        color: #595959;
+        transition: color 0.3s; /* Add a smooth transition effect */
+        letter-spacing:7px;
+    }
+
+    .social-icon:hover {
+        color: black;
+    }
+</style>
+
+<div class="col-md-6">
     <center>
-        <a href="https://www.facebook.com/sheelaabajajm" style="color:#595959;font-size:13px;padding-right:10px;padding-left:0px;" target="_blank"><i class="fab fa-facebook-f" style="color:#595959;"></i></a>
-        <a href="https://www.twitter.com/c/SheelaaMBajaj/" style="color:#595959;font-size:13px;padding-right:10px;" target="_blank"><i class="fab fa-twitter" style="color:#595959;"></i></a>
-        <a href="https://www.instagram.com/sheelaambajaj/" style="color:#595959;font-size:13px;padding-right:10px;" target="_blank"><i class="fab fa-instagram" style="color:#595959;"></i></a>
-        <a href="https://www.youtube.com/c/SheelaaMBajajOfficial" style="color:#595959;font-size:13px;padding-right:10px;" target="_blank"><i class="fab fa-youtube" style="color:#595959;"></i></a>
-    </center>
+        <a href="https://www.facebook.com/sheelaabajajm" target="_blank"><i class="fab fa-facebook-f social-icon"></i></a>
+        <a href="https://www.twitter.com/c/SheelaaMBajaj/" target="_blank"><i class="fab fa-twitter social-icon"></i></a>
+        <a href="https://www.instagram.com/sheelaambajaj/" target="_blank"><i class="fab fa-instagram social-icon"></i></a>
+        <a href="https://www.youtube.com/c/SheelaaMBajajOfficial" target="_blank"><i class="fab fa-youtube social-icon"></i></a>
+        <p>Namecraft © 2023</p>
+      </center>
 </div>
+
           <div class="col-md-3"></div>
           </div>
 
