db5e3664f66d232c7e2c693844e2196adac755ae
Uppala Hemanth <uppalahemanth4@gmail.com>
2023-08-22 12:09:06 +0530

    hemanth

--- footer.php
+++ footer.php
@@ -1,53 +1,63 @@
-<div style="background-color: #6c757d;padding:30px;margin-top:100px;" class="hide_div_desktop">
+<div style="background-color:#c0c0c0;padding:30px;margin-top:100px;" class="hide_div_desktop">
   <h1></h1>
   <footer id="footer" role="contentinfo">
     <div class="d-flex flex-row justify-content-space-between">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-md-6 col-lg-3 col-12 ">
-            <img src="images/final-logo.png" alt="Logo" style="height:80px;width:300px;">
-          </div>
-          <!-- <div class="col-md-1"></div> -->
-          <div class="col-md-6 col-lg-3 footer-widget1">
-            <h3 style="font-size:20px;color:white ;" class="pb-1">QuickLinks</h3>
+          
+
+        <div class="col-md-4 footer-widget1" >
+            <h4 style="font-size:12px;color:#61156B ;" class="pb-2">| SERVICES BY NAMECRAFT</h4><br>
             <div class="text-center" style="font-family:Open Sans, sans-serif;">
               <ul class="footer-links list-unstyled text-start">
-                <li><a href="index.php" style="margin-bottom:5px;font-size:18px;color:white">Home</a></li>
-                <li><a href="about.php" style="margin-bottom:5px;font-size:18px;color:white">About</a></li>
-                <li><a href="services.php" style="margin-bottom:5px;font-size:18px;color:white;">Services</a></li>
-                <li><a href="numbers.php" style="margin-bottom:5px;font-size:18px;color:white;">Numbers</a></li>
-                <li><a href="contact.php" style="margin-bottom:5px;font-size:18px;color:white">Contact us</a></li>
+                <li><a href="index.php" style="margin-bottom:5px;font-size:14px;color:#000000">Name Analysis</a></li><br>
+                <li><a href="about.php" style="margin-bottom:5px;font-size:14px;color:#000000">Lucky date delivery</a></li><br>
+                <li><a href="services.php" style="margin-bottom:5px;font-size:14px;color:#000000">Baby name correction</a></li><br>
+                <li><a href="numbers.php" style="margin-bottom:5px;font-size:14px;color:#000000">Lucky name correction</a></li><br>
+              </ul>
+            </div>
+          </div>
+
+          <!-- <div class="col-md-1"></div> -->
+          <div class="col-md-4 footer-widget1">
+            <h4 style="font-size:12px;color:#61156B ;" class="pb-1">| MORE INFO</h4><br>
+            <div class="text-center" style="font-family:Open Sans, sans-serif;">
+              <ul class="footer-links list-unstyled text-start">
+                <li><a href="index.php" style="margin-bottom:5px;font-size:14px;color:#000000">Terms of service</a></li><br>
+                <li><a href="about.php" style="margin-bottom:5px;font-size:14px;color:#000000"> Refund policy</a></li><br>
+                <li><a href="services.php" style="margin-bottom:5px;font-size:14px;color:#000000">Privacy policy</a></li><br>
+                <li><a href="contact.php" style="margin-bottom:5px;font-size:14px;color:#000000">Contact us</a></li><br>
               </ul>
             </div>
           </div>
           <!-- <div class="col-md-1"></div> -->
-          <div class="col-md-6 col-lg-3 footer-widget1">
-            <h3 style="font-size:20px;color:white ;" class="pb-2">Services</h3>
-            <div class="text-center" style="font-family:Open Sans, sans-serif;">
-              <ul class="footer-links list-unstyled text-start">
-                <li><a href="index.php" style="margin-bottom:5px;font-size:18px;color:white">Name Analysis</a></li>
-                <li><a href="about.php" style="margin-bottom:5px;font-size:18px;color:white">Lucky date delivery</a></li>
-                <li><a href="services.php" style="margin-bottom:5px;font-size:18px;color:white;">Baby name correction</a></li>
-                <li><a href="numbers.php" style="margin-bottom:5px;font-size:18px;color:white;">Lucky name correction</a></li>
-              </ul>
-            </div>
-          </div>
+         
           <!-- <div class="col-md-2 col-12 footer-widget1">
             <h3 style="font-size:20px;color:white;" class="pb-2">Get in touch</h3>
             <p style="font-size:18px;color:white">Number 664, 5 ft Cross, 4th block, Koramangala, Near maharaja signal, Bengaluru – 560034</p>
           </div> -->
-          <div class="col-md-6 col-lg-3 col-12 footer-widget1">
-            <!-- <ul class="fh5co-social-icons" style="font-family:Open Sans, sans-serif;"> -->
-              <h3 style="font-size:20px;color:white;padding-bottom:0px;padding-left:0px">Connect with us</h3>
-              <a href="https://www.facebook.com/sheelaabajajm" style="color:white;font-size:28px;padding-right:20px;padding-left:0px;" target="_blank"><i class="fab fa-facebook-f" style="color:#426782;"></i></a>
-              <a href="https://www.youtube.com/c/SheelaaMBajajOfficial" style="color:white;font-size:28px;padding-right:30px;" target="_blank"><i class="fab fa-youtube" style="color:red;"></i></a>
-              <a href="https://www.instagram.com/sheelaambajaj/" style="color:white;font-size:28px;padding-right:30px;" target="_blank"><i class="fab fa-instagram" style="color:#d62976;"></i></a>
-              <br>
-              <a href="tel:+91-8880030213" style="color:white;font-size:18px;padding-left:0px;">+91-8880030213</a><br>
-              <a href="mailto:info@risingphoenixdubai.com" style="color:white;font-size:18px;padding-left:0px;">info@risingphoenixdubai.com</a>
-            <!-- </ul> -->
+         
+          <div class="col-md-4  col-12 ">
+            <img src="images/final-logo.png" class="img-fluid" alt="Logo" style="height:80px;width:300px;">
           </div>
         </div>
+
+          <div class="row">
+            <div class="col-md-3"></div>
+          <div class="col-md-6   footer-widget1">
+          <center>
+            <!-- <ul class="fh5co-social-icons" style="font-family:Open Sans, sans-serif;"> -->
+              <a href="https://www.facebook.com/sheelaabajajm" style="color:#595959;font-size:13px;padding-right:10px;padding-left:0px;" target="_blank"><i class="fab fa-facebook-f" style="color:#426782;"></i></a>
+              <a href="https://www.twitter.com/c/SheelaaMBajaj/" style="color:#595959;font-size:13px;padding-right:10px;" target="_blank"><i class="fab fa-twitter" style="color:red;"></i></a>
+              <a href="https://www.instagram.com/sheelaambajaj/" style="color:#595959;font-size:13px;padding-right:10px;" target="_blank"><i class="fab fa-instagram" style="color:#d62976;"></i></a>
+              <a href="https://www.youtube.com/c/SheelaaMBajajOfficial" style="color:#595959;font-size:13px;padding-right:10px;" target="_blank"><i class="fab fa-youtube" style="color:#d62976;"></i></a>
+              
+            <!-- </ul> -->
+            </center>
+          </div>
+          <div class="col-md-3"></div>
+          </div>
+
       </div>
     </div>
   </footer>
