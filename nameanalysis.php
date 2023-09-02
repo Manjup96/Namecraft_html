f88c21bbe4abfa8c1e06a7af618ec6b9cb2e04b7
Uppala Hemanth <uppalahemanth4@gmail.com>
2023-09-02 13:52:09 +0530

    hemanth

--- nameanalysis.php
+++ nameanalysis.php
@@ -205,7 +205,7 @@
 
             <div class="col-lg-6"  style="margin-top:50px;">
             <p class="text" style=" font-size:36px;">Name Analysis</p>
-            <h2 class="text" style="font-family:Mulish,Arial,sans-serif;font-size: 26px;">Rs:500.00</h2>
+            <h2 class="text" style=" font-family: montserrat;font-size: 26px;">Rs:500.00</h2>
             <p style="text-align: justify; font-size: 16px; font-family:montserrat;"> Name analysis in numerology entails analysing the letters of a person’s name
                      to get insight into their personality, attributes, and probable life events.
                      Numerologists think that each letter contains a distinct vibration or energy, 
@@ -360,19 +360,19 @@ they may learn about a person’s personality and life path.</p>
             <div class="col-lg-4 col-md-6 col-sm-10 " style="margin-top:10px">
             <a href="luckydate.php"> <img src="images/C section delivery date.jpg" style="width:350px;height:350px; border: 2px solid black;"></a>
             <h2 class="text-center mt-3" style="font-size: 16px; font-family:montserrat; font-color:#595959;">Lucky C-Section Date</h2>
-            <p class="text-center" style="font-size: 16px;">Rs:3,000.00</p>
+            <p class="text-center" style="font-size: 16px; font-family: montserrat;">Rs:3,000.00</p>
             </div>
 
             <div class="col-lg-4 col-md-6 col-sm-10"  style="margin-top:10px">
             <a href="babyname.php"><img src="images/babyname numerology.jpg" style="width:350px;height:350px; border: 2px solid black;"></a>
             <h2 class="text-center mt-3" style="font-size: 16px; font-family:montserrat; font-color:#595959;">Lucky Baby Name</h2>
-            <p class="text-center" style="font-size: 16px;">Rs:7,500.00</p>
+            <p class="text-center" style="font-size: 16px; font-family: montserrat;">Rs:7,500.00</p>
             </div>
 
             <div class="col-lg-4 col-md-6 col-sm-10" style="margin-top:10px">
             <a href="luckyname.php"><img src="images/LUCKY ALPHABETS.jpg" style="width:350px;height:350px; border: 2px solid black;"></a>
             <h2 class="text-center mt-3" style="font-size: 16px; font-family:montserrat; font-color:#595959;">Lucky Name Correction</h2>
-            <p class="text-center" style="font-size: 16px;">Rs:7,500.00</p>
+            <p class="text-center" style="font-size: 16px; font-family: montserrat;">Rs:7,500.00</p>
             </div>
 </div>
 </div>
